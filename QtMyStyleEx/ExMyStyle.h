ddd9a2f72f895092d4de5f9ea3beab92084a46e6
touwoyimuli <touwoyimuli@gmail.com>
2020-01-07 20:09:03 +0800

    feat: 创建 ExMyStyle 一个空的Qt的工程，用来编码自定义控件（操作系统层次的重新绘画控件）

---
+++ QtMyStyleEx/ExMyStyle.h
@@ -0,0 +1,33 @@
+/*
+ * Author:  touwoyimuli <touwoyimuli@gmai.com>
+ * blogs:   https://touwoyimuli.github.io/
+ *
+ *             Copyright 2019 touwoyimuli
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef EXMYSTYLE_H
+#define EXMYSTYLE_H
+
+#include <QDialog>
+
+class ExMyStyle : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ExMyStyle(QWidget *parent = 0);
+    ~ExMyStyle();
+};
+
+#endif // EXMYSTYLE_H
