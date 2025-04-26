0f9a34a504aab79c25e560b27060b722192d04d6
XMuli <xmulitech@gmail.com>
2025-04-26 20:22:07 +0800

    feat: gcc 和 g++的区别

---
+++ unix_linux_02_c_cpp/main.c
@@ -0,0 +1,11 @@
+//  Created by muli on 2020/3/2.
+//  Copyright © 2020 muli. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
