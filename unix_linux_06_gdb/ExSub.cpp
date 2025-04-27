cc60b2eb43466d6bd7f47c63b25181110d5ad510
XMuli <xmulitech@gmail.com>
2025-04-27 21:28:50 +0800

    feat: 用 gdb 进行调试

---
+++ unix_linux_06_gdb/ExSub.cpp
@@ -0,0 +1,14 @@
+//
+//  ExSub.cpp
+//  test
+//
+//  Created by muli on 2020/3/2.
+//  Copyright © 2020 muli. All rights reserved.
+//
+
+#include <stdio.h>
+
+double sub(double a, double b)
+{
+    return a - b;
+}
