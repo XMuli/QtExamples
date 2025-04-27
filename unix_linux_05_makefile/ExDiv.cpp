12b27490c404ca1150a31734e790d47d81ddb285
XMuli <xmulitech@gmail.com>
2025-04-27 21:28:27 +0800

    feat: 编写 makefile 文件

---
+++ unix_linux_05_makefile/ExDiv.cpp
@@ -0,0 +1,14 @@
+//
+//  ExDiv.cpp
+//  test
+//
+//  Created by muli on 2020/3/2.
+//  Copyright © 2020 muli. All rights reserved.
+//
+
+#include <stdio.h>
+
+double div(double a, double b)
+{
+    return a * 1.0 / b;
+}
---
+++ unix_linux_05_makefile/ExMul.cpp
@@ -0,0 +1,14 @@
+//
+//  ExMul.cpp
+//  test
+//
+//  Created by muli on 2020/3/2.
+//  Copyright © 2020 muli. All rights reserved.
+//
+
+#include <stdio.h>
+
+double mul(double a, double b)
+{
+    return a * b;
+}
